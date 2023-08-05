3206e062a7994e45b66416508bb01012f43fd7f9
Iain Cameron <icameron00@gmail.com>
2023-08-05 22:09:57 +1200

    chore: remove unused files

--- packages/node-lambda/src/utils/typescript/ensure-typescript.ts
+++
@@ -1,9 +0,0 @@
-import { ensurePackage } from '@nx/devkit';
-import { typescriptVersion } from '../versions';
-
-export function ensureTypescript() {
-  return ensurePackage<typeof import('typescript')>(
-    'typescript',
-    typescriptVersion
-  );
-}
